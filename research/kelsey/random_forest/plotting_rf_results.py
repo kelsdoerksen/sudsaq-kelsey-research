fixing normalization

research/kelsey/random_forest/plotting_rf_results.py
@@ -45,6 +45,7 @@ def get_args():
     parser.add_argument('--target', help='Target. mda8 or bias')
     parser.add_argument('--month', help='Month of analysis')
     parser.add_argument('--n_channels', help='Number of channels for model')
+    parser.add_argument('--root_dir', help='Root directory of runs')
     parser.add_argument('--save_dir', help='Save Directory for run', required=True)
     parser.add_argument('--wandb_run', help='Wandb run', required=True)
     return parser.parse_args()
@@ -139,7 +140,7 @@ def plot_histogram(target, pred, dir, region, analysis_time, wandb_run):
     Plot histogram of true vs predicted
     '''
 
-    bins = np.linspace(-120, 120, 300)
+    bins = np.linspace(-120, 100, 300)
     plt.hist(target, bins, histtype='step', label=['target'])
     plt.hist(pred, bins, histtype='step', label=['prediction'])
     plt.legend(loc='upper right')
@@ -223,7 +224,7 @@ def plotting_spatial_data(avg_data, metric, model_target, anaylsis_date, save_di
             vmin = -25
             vmax = 25
         else:
-            vmin = -20
+            vmin = -5
             vmax = 50
 
     # fixing plotting upside down
@@ -418,8 +419,11 @@ if __name__ == '__main__':
     target = args.target
     n_channels = args.n_channels
     month = args.month
+    root_dir = args.root_dir
     save_dir = args.save_dir
     wandb_run = args.wandb_run
+
+    save_dir = root_dir + '/' + save_dir
 
     # Subset yhat (predictions), y_true (groundtruth)
     results_df = pd.read_csv('{}/{}_prediction_groundtruth_rmse.csv'.format(save_dir, wandb_run))

research/kelsey/unet/analysis/plotting_results.py
@@ -462,6 +462,8 @@ def spatial_map(avg_data, target, metric, region, savedir):
                 vmax = 80
             if region == 'NorthAmerica':
                 vmax = 200
+            if region == 'Globe':
+                vmax = 200
             vmin = 10
         elif metric == 'upper_bound_predictions':
             vmin = 20
@@ -476,7 +478,8 @@ def spatial_map(avg_data, target, metric, region, savedir):
             vmin = -5
             vmax = 50
 
-    x, y = np.meshgrid(lon_vals, lat_vals, indexing='xy')
+    lat_plot = lat_vals[::-1]
+    x, y = np.meshgrid(lon_vals, lat_plot, indexing='xy')
     fig, ax = plt.subplots(figsize=(12, 10), subplot_kw={'projection': ccrs.PlateCarree()})
     plt.pcolor(x, y, avg_data, cmap='coolwarm', vmin=vmin, vmax=vmax)
     ax.coastlines(linewidth=2)

research/kelsey/unet/run_pipeline.py
@@ -129,40 +129,18 @@ if __name__ == '__main__':
     std = torch.zeros(n_channels)
     global_min = torch.full((n_channels,), float('inf'))
     global_max = torch.full((n_channels,), float('-inf'))
-    n_samples = 0
 
     if args.norm == 'zscore':
-        print("Computing per-channel mean and std...")
+        print("Computing global per-channel mean and std (exact)...")
+        all_images = []
         for images, _ in aq_train_dataset:
-            images = images.float().contiguous()
-            # Reshape to (32, 6000)
-            images_flat = images.view(n_channels, -1)
-
-            # Calculate mean and std
-            mean += images_flat.mean(dim=1)
-            std += images_flat.std(dim=1)
-            n_samples += 1
-
-        mean /= n_samples
-        std /= n_samples
+            all_images.append(images.float().contiguous())
+        all_images = torch.stack(all_images)  # shape [N, C, H, W]
+        mean = all_images.mean(dim=(0, 2, 3))
+        std = all_images.std(dim=(0, 2, 3))
         max_val = None
         min_val = None
 
-    if args.norm == 'minmax':
-        print("Computing per-channel min and max...")
-        for images, _ in aq_train_dataset:
-            images = images.float().contiguous()
-            # Reshape to (32, 6000)
-            images_flat = images.view(n_channels, -1)
-
-            # Calculating min and max
-            min_vals = images_flat.min(dim=1).values
-            max_vals = images_flat.max(dim=1).values
-
-            global_min = torch.min(global_min, min_vals)
-            global_max = torch.max(global_max, max_vals)
-            mean = None
-            std = None
 
     aq_train_dataset = AQDataset(sample_dir_root, label_dir_root, 'train', analysis_month, test_year, channels,
                                  region, sensitivity_feature, mean=mean, std=std, max_val= max_val, min_val= min_val,